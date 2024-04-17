9e380eda59a481e6c07121902fd7fb997c71fc67
pablothestrongestavenger <138133672+pablothestrongestavenger@users.noreply.github.com>
2024-04-18 00:24:32 +0800

    Updates in UI and initial functions

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <div id="app">
+    <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#productOffcanvas" aria-controls="productOffcanvas">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
+    <div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="productOffcanvas" aria-labelledby="productOffcanvasLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="productOffcanvasLabel">Product Menu</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <AddProduct @add="addProduct" />
+      </div>
+    </div>
+    <ProductList :products="products" />
+    <EditProduct v-if="editedProduct" :product="editedProduct" @edit="saveEditedProduct" />
+  </div>
 </template>
 
 <script>
+import ProductList from './components/ProductList.vue'
+import AddProduct from './components/AddProduct.vue'
+import EditProduct from './components/EditProduct.vue'
+
 export default {
-  name: 'App',
-  
+  components: {
+    ProductList,
+    AddProduct,
+    EditProduct
+  },
+  data() {
+    return {
+      products: [
+        { id: 1, name: 'Product 1', description: 'Description 1', price: 10 },
+        { id: 2, name: 'Product 2', description: 'Description 2', price: 20 }
+      ],
+      editedProduct: null
+    }
+  },
+  methods: {
+    addProduct(newProduct) {
+      this.products.push({ ...newProduct, id: this.products.length + 1 })
+      // Add animation logic here
+    },
+    editProduct(product) {
+      this.editedProduct = product
+    },
+    saveEditedProduct(editedProduct) {
+      const index = this.products.findIndex(p => p.id === editedProduct.id)
+      if (index !== -1) {
+        this.products.splice(index, 1, editedProduct)
+        // Add animation logic here
+        this.editedProduct = null
+      }
+    },
+    deleteProduct(id) {
+      const index = this.products.findIndex(p => p.id === id)
+      if (index !== -1) {
+        this.products.splice(index, 1)
+        // Add animation logic here
+      }
+    }
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/EditProduct.vue
@@ -0,0 +1,29 @@
+
+<template>
+    <div>
+      <h2>Edit Product</h2>
+      <form @submit.prevent="editProduct">
+        <input type="text" v-model="editedProduct.name">
+        <input type="text" v-model="editedProduct.description">
+        <input type="number" v-model="editedProduct.price">
+        <button type="submit">Save Changes</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['product'],
+    data() {
+      return {
+        editedProduct: { ...this.product }
+      }
+    },
+    methods: {
+      editProduct() {
+        this.$emit('edit', this.editedProduct)
+      }
+    }
+  }
+  </script>
+  
